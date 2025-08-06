8876b62201e1e9c70d399cd45a9f80270a7a0808
Abdullah Oday <abdallaoday7@hotmail.com>
2025-08-06 22:40:28 +0300

    Forgot the fucking email data annotation

--- AuthService/Request/RegisterRequest.cs
+++ AuthService/Request/RegisterRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace AuthService.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Request
 {
     public class RegisterRequest
     {
+        [EmailAddress]
         public required string UserEmail { get; set; }
         public required string Password { get; set; }
         public required string UserName { get; set; }
