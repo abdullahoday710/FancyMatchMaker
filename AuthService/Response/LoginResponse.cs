8df737cded1d5fb667817ea105d7de237c070919
Abdullah Oday <abdallaoday7@hotmail.com>
2025-08-08 16:42:21 +0300

    Added a login response class

---
+++ AuthService/Response/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthService.Response
+{
+    public class LoginResponse
+    {
+        public required string Email { get; set; }
+        public required string UserName { get; set; }
+        public required string AuthToken { get; set; }
+    }
+}
